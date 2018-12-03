499aa33b0d8ab71e1a4f3bcdcfcb420e4b8d5703
pourso <43544872+pourso@users.noreply.github.com>
2018-12-03 07:14:47 -0800

    Add files via upload

--- final_project.R
+++ final_project.R
@@ -1,6 +1,7 @@
 library(ggplot2)
+library(rstudioapi) 
 
-# 1. simulation study
+##### 1. simulation study
 
 # set random number generator seed
 set.seed(1981)
@@ -10,10 +11,12 @@ n_sim <- 1000
 ns <- c(10, 100, 1000)
 
 # read in data
-yrbss_2003 <- readRDS("mod9/yrbss_2003.rds")
-yrbss_2013 <- readRDS("mod9/yrbss_2013.rds")
+current_path <- getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
+yrbss_2003 <- readRDS("yrbss_2003.rds")
+yrbss_2013 <- readRDS("yrbss_2013.rds")
 
-# 1.a. describe sampling distribution of sample mean of 2013 BMI data
+##### 1.a. describe sampling distribution of sample mean of 2013 BMI data
 # function to generate sample means
 create_sample_mean <- function(n, n_sim){
   replicate(n_sim, mean(sample(x=yrbss_2013[["bmi"]],size=n)))
@@ -54,99 +57,139 @@ report_summary <- function(fn,file_string,x_label){
 }
 
 # call function for sample means
-report_summary(fn=create_sample_mean,file_string="mod9/plot1a.rds",x_label = "means")
+report_summary(fn=create_sample_mean,file_string="plot1a.rds",x_label = "means")
 
-#        n_lo   n_md   n_hi
-# [1,] 23.642 23.642 23.643
-# [2,]  1.563  0.498  0.151
-
-# As can be seen from the output of rbind(), the mean of the sampling distribution is fairly stable
-# as sample size increases from 10 to 100 to 1000, but the standard deviation dramatically decreases 
-# with increasing sample size. This is illustrated in the histogram overlay, wherein the mean +- standard
-# deviation is indicated by dashed and dotted lines of the corresponding color.
-
-# 1.b. sample quantiles: 25th percentile
+##### 1.b. sample quantiles: 25th percentile
 create_sample_quant <- function(n, n_sim){
   replicate(n_sim, quantile(sample(x=yrbss_2013[["bmi"]],size=n), probs=c(0.25)))
 }
-report_summary(fn=create_sample_quant,file_string="mod9/plot1b.rds",x_label = "quants")
-#        n_lo   n_md   n_hi
-# [1,] 20.629 20.301 20.274
-# [2,]  1.308  0.407  0.128
+report_summary(fn=create_sample_quant,file_string="plot1b.rds",x_label = "quants")
 
-# As can be seen from the returned values, the 25th percentile of the sampling distribution is fairly stable
-# as sample size increases from 10 to 100 to 1000, but the standard deviation dramatically decreases 
-# with increasing sample size. This is illustrated in the histogram overlay, where the spread of the dist 
-# is clearly decreasing as the sample size increases, but are still roughly centered around the same
-# value.
-
-# 1.c. sample minimum
+##### 1.c. sample minimum
 create_sample_min <- function(n, n_sim){
   replicate(n_sim, min(sample(x=yrbss_2013[["bmi"]],size=n)))
 }
-report_summary(fn=create_sample_min,file_string="mod9/plot1c.rds",x_label = "mins")
-#        n_lo  n_md   n_hi
-# [1,] 18.091 15.63 14.040
-# [2,]  1.480  0.96  0.581
+report_summary(fn=create_sample_min,file_string="plot1c.rds",x_label = "mins")
 
-# In the case of the sample minimum, the standard deviation continues the expected trend: the values decrease
-# with increasing sample size. However, in the case of the summary statistic, the mean value decreases
-# significantly. This is visualized in the histogram overlay as the distributions are no longer centered
-# around the same approximate value. Furthermore, the distribution corresponding to the largest sample size 
-# (green, n = 1000) is truncated. This is intuitive, as we'd expect the summary statistic to approach the 
-# population minimum as sample size increases.
-
-# 1.d. difference in sample median BMI
+##### 1.d. difference in sample median BMI
 # sample stat is difference of medians
 # update sample size
 ns <- c(5, 10, 100)
 create_sample_median_diff <- function(n, n_sim){
   replicate(n_sim, median(sample(x=yrbss_2013[["bmi"]],size=n))-median(sample(x=yrbss_2003[["bmi"]],size=n)) )
 }
-report_summary(fn=create_sample_median_diff,file_string="mod9/plot1d.rds",x_label = "median's diff")
-#       n_lo  n_md  n_hi
-# [1,] 0.237 0.131 0.181
-# [2,] 3.115 2.122 0.686
+report_summary(fn=create_sample_median_diff,file_string="plot1d.rds",x_label = "median's diff")
 
-# The difference in median BMI continues to exhibit the expected trend in standard deviation, decreasing
-# as sample size increases. The summary statistic is approximately normal and roughly centered around
-# the same value. Given that the median is simply the 50th percetile, this would be expected for a single 
-# median. The histogram overlay suggests that the difference in normal distributions is also approximately 
-# normal.
-
-# 1.e.
-#> report_summary(fn=create_sample_mean,file_string="mod9/plot1a.rds",x_label = "means")
+##### 1.e.
+#> report_summary(fn=create_sample_mean,file_string="plot1a.rds",x_label = "means")
 #       n_lo   n_md   n_hi
 #[1,] 23.642 23.642 23.643
 #[2,]  1.563  0.498  0.151
-readRDS("mod9/plot1a.rds")
+readRDS("plot1a.rds")
 
-#> report_summary(fn=create_sample_quant,file_string="mod9/plot1b.rds",x_label = "quants")
+#> report_summary(fn=create_sample_quant,file_string="plot1b.rds",x_label = "quants")
 #       n_lo   n_md   n_hi
 #[1,] 20.629 20.301 20.274
 #[2,]  1.308  0.407  0.128
-readRDS("mod9/plot1b.rds")
+readRDS("plot1b.rds")
 
-#> report_summary(fn=create_sample_min,file_string="mod9/plot1c.rds",x_label = "mins")
+#> report_summary(fn=create_sample_min,file_string="plot1c.rds",x_label = "mins")
 #       n_lo  n_md   n_hi
 #[1,] 18.091 15.63 14.040
 #[2,]  1.480  0.96  0.581
-readRDS("mod9/plot1c.rds")
+readRDS("plot1c.rds")
 
-#> report_summary(fn=create_sample_median_diff,file_string="mod9/plot1d.rds",x_label = "median's diff")
+#> report_summary(fn=create_sample_median_diff,file_string="plot1d.rds",x_label = "median's diff")
 #     n_lo  n_md  n_hi
 #[1,] 0.237 0.131 0.181
 #[2,] 3.115 2.122 0.686
-readRDS("mod9/plot1d.rds")
+readRDS("plot1d.rds")
 
-# In all cases of the simulation study, the standard deviations of the summary statistic decreases
-# with increasing sample size. For the sample mean, this is understood from the Central Limit Theorem
-# as the variance of the sample distribution is equal to the population variance divided by the sample size.
-# Hence, as sample size increases, the variance decreases.
-# The sampling distribution itself is apparently normal for the sample mean, again consistent with 
-# the CLT. This appears to be true of the 0.25 quantile as well, although for seemingly more advanced
-# reasons that laid out thus far in the coursework. The difference in medians also exhibits an approximately
-# normal distribution. 
-# The sample minimum, however, doesn't exhibit the same general behavior as the other summary statistics: as 
-# sample size is increased, a greater range of values is included in the sample, and hence a the probability
-# of a lower minimum increases. This is evident by the rightward shift of the mean sample minimum.
+##### 2. data analysis
+
+##### 2.a. how has BMI changed?
+
+# inspect data
+qplot(yrbss_2003[["bmi"]])
+qplot(yrbss_2013[["bmi"]])
+
+# clean data?
+anyNA(yrbss_2003[["bmi"]])
+anyNA(yrbss_2013[["bmi"]])
+
+# consider sample means of BMI data
+mean(yrbss_2003[["bmi"]])
+mean(yrbss_2013[["bmi"]])
+
+prob_2a <- t.test(x = yrbss_2013[["bmi"]], 
+                  y = yrbss_2003[["bmi"]],
+                  alternative = "g")
+prob_2a
+
+# 2.b. potential for smoking?
+
+# first approach: classify observations as smokers and non-smokers
+
+#create new column to indicate frequency of smoking ("smoker")
+q33_levels <- c("0 days", "1 or 2 days", "3 to 5 days", "6 to 9 days", "10 to 19 days", "20 to 29 days", "All 30 days")
+yrbss_2013$smoke = as.integer(factor(yrbss_2013[["q33"]],levels = q33_levels))
+
+# create new column indicating class membership - non-smoker:0, smoker:1
+yrbss_2013$is_smoker = 0
+yrbss_2013[is.na(yrbss_2013$smoke),"is_smoker"] <- NA
+yrbss_2013$is_smoker[yrbss_2013$smoke < 2] <- 0
+yrbss_2013$is_smoker[yrbss_2013$smoke > 1] <- 1
+
+# inspect data
+barplot(table(yrbss_2013[yrbss_2013$sex=="Male","is_smoker"]),names.arg = c("non-smoker", "smoker"),
+        main = "Male smoker status", xlab = "Class", ylab = "Count")
+barplot(table(yrbss_2013[yrbss_2013$sex=="Female","is_smoker"]),names.arg = c("non-smoker", "smoker"),
+        main = "Female smoker status",xlab = "Class", ylab = "Count")
+
+# first approach: test for proportions
+m<-table(yrbss_2013[yrbss_2013$sex=="Male","is_smoker"])
+f<-table(yrbss_2013[yrbss_2013$sex=="Female","is_smoker"])
+
+rbind(prop.table(m),prop.table(f))
+
+# 2-sample test for equal proportions
+sm <- c(m[2],f[2])
+total <- c(m[1]+m[2],f[1]+f[2])
+prop.test(sm,total, correct = FALSE)
+
+# second approach: convert categorical responses to ordinal data
+
+q33_t <- table(yrbss_2013[["smoke"]])
+barplot(q33_t)
+
+q33_nick <- c("0","1 or 2","3 to 5","6 to 9","10 to 19","20 to 29","30")
+mf<-table(yrbss_2013[yrbss_2013$sex=="Male","smoke"])
+ff<-table(yrbss_2013[yrbss_2013$sex=="Female","smoke"])
+rbind(round(prop.table(mf),3),round(prop.table(ff),3))
+barplot(table(yrbss_2013[yrbss_2013$sex=="Male","q33"]),names.arg = q33_nick,
+        main = "Days male high-schoolers smoked in last 30",
+        xlab = "Days", ylab = "Count")
+barplot(table(yrbss_2013[yrbss_2013$sex=="Female","q33"]),names.arg = q33_nick,
+        main = "Days female high-schoolers smoked in last 30",
+        xlab = "Days smoked in last 30", ylab = "Count")
+
+# chi-squared test requires count of new categorical data (male) and expected proportions (female)
+chisq.test(table(x = yrbss_2013[yrbss_2013$sex=="Male","smoke"]),
+           p = prop.table(table(yrbss_2013[yrbss_2013$sex=="Female","smoke"])),
+           correct = FALSE)
+
+# 2.c. how much TV do high-schoolers watch?
+
+# inspect data as simple table
+table(yrbss_2013[,"q81"])
+
+# plot as bar chart
+q81_nick <- c("0","<1","1","2","3","4","5+")
+barplot(table(yrbss_2013[,"q81"]),names.arg = q81_nick,
+        main = "Average hours of TV on school day", xlab = "Hours", ylab = "Count")
+
+# create new column with ordinal data, review summary of proportions
+yrbss_2013$tv = as.integer(factor(yrbss_2013[["q81"]]))
+tv_table <- t(as.matrix(prop.table(table(yrbss_2013[,"tv"])))) 
+colnames(tv_table) <- q81_nick
+round(tv_table,3)
